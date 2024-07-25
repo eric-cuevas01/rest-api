937e41a08c4b19f909686bb082bd47955c42f058
Eric Cuevas <eric.cuevas01@student.csulb.edu>
2024-07-24 21:08:26 -0700

    set up mongodb

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -20,3 +21,10 @@ const server = http.createServer(app);
 server.listen(8080, () => {
     console.log('Server running on http://localhost:8080/');
 });
+
+const MONGO_URL = 'mongodb+srv://ericcuevas01:XWGo9ftyB3hb3MRF@cluster0.dqeemm2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
+ 
